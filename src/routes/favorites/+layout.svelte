a2fce6552820281f6ce0ea32ab964d35a692eaa3
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-06-03 18:25:53 -0700

    Show message for no favorites

--- src/routes/favorites/+layout.svelte
+++ src/routes/favorites/+layout.svelte
@@ -32,43 +32,49 @@
 </script>
 
 <h1 class="text-4xl font-bold mb-4">Favorites</h1>
-<form class="space-y-2 mb-4" bind:this={form} on:change={() => form.requestSubmit()}>
-	<!-- TODO: no JS version? -->
-	<label class="flex items-center space-x-2">
-		<input class="radio" type="radio" name="groupBy" value="album" bind:group={groupBy} />
-		<span>Album</span>
-	</label>
-	<label class="flex items-center space-x-2">
-		<input class="radio" type="radio" name="groupBy" value="artist" bind:group={groupBy} />
-		<span>Artist</span>
-	</label>
-</form>
 
-<div class="grid favorites gap-6">
-	<ul class="rounded-md border-4 border-primary-900 self-start">
-		{#each items as favorite}
-			{@const isSelected = $page.url.pathname === favorite.path}
-			<li
-				class="grid grid-cols-3 gap-2 items-center p-2 relative {isSelected
-					? 'bg-surface-900'
-					: 'bg-primary-900 hover:bg-surface-900'}"
-			>
-				<img src={favorite.image} alt="" class="hidden sm:block" />
-				<a
-					href={favorite.path}
-					class="col-span-full sm:col-span-2 text-base sm:text-xl leading-snug no-underline after:absolute after:inset-0"
-					><span class:font-bold={isSelected}>{favorite.title}</span> <br />
-					{#if favorite.subtitle}
-						<span class="text-sm">{favorite.subtitle}</span>{/if}</a
+{#if items.length === 0}
+	<p class="text-xl">
+		You haven't selected any favorites. Maybe <a href="/search">search</a> for some?
+	</p>
+{:else}
+	<form class="space-y-2 mb-4" bind:this={form} on:change={() => form.requestSubmit()}>
+		<!-- TODO: no JS version? -->
+		<label class="flex items-center space-x-2">
+			<input class="radio" type="radio" name="groupBy" value="album" bind:group={groupBy} />
+			<span>Album</span>
+		</label>
+		<label class="flex items-center space-x-2">
+			<input class="radio" type="radio" name="groupBy" value="artist" bind:group={groupBy} />
+			<span>Artist</span>
+		</label>
+	</form>
+	<div class="grid favorites gap-6">
+		<ul class="rounded-md border-4 border-primary-900 self-start">
+			{#each items as favorite}
+				{@const isSelected = $page.url.pathname === favorite.path}
+				<li
+					class="grid grid-cols-3 gap-2 items-center p-2 relative {isSelected
+						? 'bg-surface-900'
+						: 'bg-primary-900 hover:bg-surface-900'}"
 				>
-			</li>
-		{/each}
-	</ul>
+					<img src={favorite.image} alt="" class="hidden sm:block" />
+					<a
+						href={favorite.path}
+						class="col-span-full sm:col-span-2 text-base sm:text-xl leading-snug no-underline after:absolute after:inset-0"
+						><span class:font-bold={isSelected}>{favorite.title}</span> <br />
+						{#if favorite.subtitle}
+							<span class="text-sm">{favorite.subtitle}</span>{/if}</a
+					>
+				</li>
+			{/each}
+		</ul>
 
-	<div>
-		<slot />
+		<div>
+			<slot />
+		</div>
 	</div>
-</div>
+{/if}
 
 <style>
 	.favorites {
--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -1,1 +1,1 @@
-<div class="grid place-items-center text-3xl font-bold"><p>Nothing selected!</p></div>
+<p class="text-3xl font-bold">Nothing selected!</p>
