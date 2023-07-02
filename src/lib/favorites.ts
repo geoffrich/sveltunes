70a9a665491d1a989a5dad98371da5d716bdf842
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-07-02 15:56:28 -0700

    Remove set/update from custom store

--- src/lib/favorites.ts
+++ src/lib/favorites.ts
@@ -1,13 +1,15 @@
 import { type Writable, writable, type Updater } from 'svelte/store';
 import type { AlbumDetail } from './types';
 
-export interface FavoritesStore extends Writable<AlbumDetail[]> {
+export interface FavoritesStore {
+	// a subscribe method that is the same type as the Writable subscribe method
+	subscribe: Writable<AlbumDetail[]>['subscribe'];
 	remove: (id: number) => () => void;
 	get: (albumId: string) => AlbumDetail | undefined;
 	getArtistAlbums: (artistId: string) => AlbumDetail[];
 }
 
-export function createFavoritesStore(favorites: AlbumDetail[]) {
+export function createFavoritesStore(favorites: AlbumDetail[]): FavoritesStore {
 	const _store = writable(favorites);
 	let _favorites = favorites;
 	const subscribe = _store.subscribe;
@@ -22,8 +24,6 @@ export function createFavoritesStore(favorites: AlbumDetail[]) {
 
 	const store = {
 		subscribe,
-		set,
-		update,
 		remove: (id: number) => {
 			const toRemove = _favorites.findIndex((f) => f.id === id);
 			if (toRemove === -1)
